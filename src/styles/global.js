244424c8128008c577ecaaab8e7992f56fbdcc02
Daniel Garcia <daniel.of@gmail.com>
2020-05-25 17:30:23 -0300

    starting the project

---
+++ src/styles/global.js
@@ -0,0 +1,26 @@
+import {createGlobalStyle} from 'styled-components';
+
+export default createGlobalStyle`
+
+  * {
+    padding: 0;
+    margin: 0;
+    outline: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    height: 100vh;
+    font-family: 'Public Sans', sans-serif;
+    font-size: 18px;
+  }
+
+  #root {
+    height: 100%;
+  }
+
+  button {
+    cursor: pointer;
+  }
+
+`;
